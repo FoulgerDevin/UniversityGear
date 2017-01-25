f3ba9032e41b982926372c91808835436dcfcef7
Hector Trujillo <hector.trujillo@oregonstate.edu>
2017-01-24 21:57:37 -0800

    Starts development of #6

---
+++ UniversityGear/app/src/main/java/university4credit/universitygear/DisplaySearchResultsActivity.java
@@ -0,0 +1,26 @@
+package university4credit.universitygear;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class DisplaySearchResultsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_search_results);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(SearchActivity.EXTRA_MESSAGE);
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+
+        ViewGroup layout = (ViewGroup) findViewById(R.id.activity_display_search_results);
+        layout.addView(textView);
+
+    }
+}
--- UniversityGear/app/src/main/java/university4credit/universitygear/SearchActivity.java
+++ UniversityGear/app/src/main/java/university4credit/universitygear/SearchActivity.java
@@ -1,10 +1,12 @@
 package university4credit.universitygear;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Context;
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -18,6 +20,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class SearchActivity extends AppCompatActivity {
+    public final static String EXTRA_MESSAGE = "university4credit.universitygear.MESSAGE";
     Button mButton;
     EditText mEdit;
     TextView mText;
@@ -54,12 +57,11 @@ public class SearchActivity extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_search);
         mButton = (Button)findViewById(R.id.button1);
         RadioGroup radioGroup = (RadioGroup) findViewById(R.id.radiogroup);
         radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
 
-            @Override
             public void onCheckedChanged(RadioGroup group, int checkedId)
             {
                 RadioButton checkedRadioButton = (RadioButton) findViewById(checkedId);
@@ -67,47 +69,98 @@ public class SearchActivity extends AppCompatActivity {
             }
         });
 
-        mButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                mEdit   = (EditText)findViewById(R.id.editText1);
-                mText = (TextView)findViewById(R.id.textView1);
-                String [] ParsedWords = mEdit.getText().toString().split("\\s+") ;
-                for(int x = 0; x < ParsedWords.length;x++){
-                    mindistance = 3;
-                    for(int y = 0; y < database.length; y++){
-                        if(distance(ParsedWords[x],database[y])<2){
-                            tempstring = database[y];
-                            break;
-                        }
-                        else if(distance(ParsedWords[x],database[y])<mindistance){
-                            tempstring = database[y];
-                        }
-                        else tempstring = ParsedWords[x];
-                    }
-                    ParsedWords[x]=tempstring;
-                }
-
-                join = ParsedWords[0];
-                for(int x = 1; x < ParsedWords.length;x++){
-                    join += " "; join+=ParsedWords[x];
-                }
-
-                FileOutputStream stream = null;
-                try {
-                    stream = openFileOutput("history.txt", Context.MODE_APPEND);
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
-                }
-
-                try {
-                    stream.write(join.getBytes());
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                if( mEdit.getText().toString().length()!=0)
-                    mText.setText("You are looking for  "+ temp +" "+join);
-            }
-        });
-
+//        mButton.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View view) {
+//                mEdit   = (EditText)findViewById(R.id.editText1);
+//                mText = (TextView)findViewById(R.id.textView1);
+//                String [] ParsedWords = mEdit.getText().toString().split("\\s+") ;
+//                for(int x = 0; x < ParsedWords.length;x++){
+//                    mindistance = 3;
+//                    for(int y = 0; y < database.length; y++){
+//                        if(distance(ParsedWords[x],database[y])<2){
+//                            tempstring = database[y];
+//                            break;
+//                        }
+//                        else if(distance(ParsedWords[x],database[y])<mindistance){
+//                            tempstring = database[y];
+//                        }
+//                        else tempstring = ParsedWords[x];
+//                    }
+//                    ParsedWords[x]=tempstring;
+//                }
+//
+//                join = ParsedWords[0];
+//                for(int x = 1; x < ParsedWords.length;x++){
+//                    join += " "; join+=ParsedWords[x];
+//                }
+//
+//                FileOutputStream stream = null;
+//                try {
+//                    stream = openFileOutput("history.txt", Context.MODE_APPEND);
+//                } catch (FileNotFoundException e) {
+//                    e.printStackTrace();
+//                }
+//
+//                try {
+//                    stream.write(join.getBytes());
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//                if( mEdit.getText().toString().length()!=0)
+//                    mText.setText("You are looking for  "+ temp +" "+join);
+//            }
+//        });
+//
     }
+
+    public void search(View view) {
+        Intent intent = new Intent(this, DisplaySearchResultsActivity.class);
+        EditText searchText = (EditText) findViewById(R.id.editText1);
+        String message = searchText.getText().toString();
+
+        mEdit   = (EditText)findViewById(R.id.editText1);
+        mText = (TextView)findViewById(R.id.textView1);
+        String [] ParsedWords = mEdit.getText().toString().split("\\s+") ;
+        for(int x = 0; x < ParsedWords.length;x++){
+            mindistance = 3;
+            for(int y = 0; y < database.length; y++){
+                if(distance(ParsedWords[x],database[y])<2){
+                    tempstring = database[y];
+                    break;
+                }
+                else if(distance(ParsedWords[x],database[y])<mindistance){
+                    tempstring = database[y];
+                }
+                else tempstring = ParsedWords[x];
+            }
+            ParsedWords[x]=tempstring;
+        }
+
+         join = ParsedWords[0];
+         for(int x = 1; x < ParsedWords.length;x++){
+            join += " "; join+=ParsedWords[x];
+         }
+
+         FileOutputStream stream = null;
+         try {
+            stream = openFileOutput("history.txt", Context.MODE_APPEND);
+         } catch (FileNotFoundException e) {
+            e.printStackTrace();
+         }
+
+         try {
+            stream.write(join.getBytes());
+         } catch (IOException e) {
+            e.printStackTrace();
+         }
+         if( mEdit.getText().toString().length()!=0) {
+             //I believe we can enforce this so that a user can't hit submit until they've typed in something.
+             mText.setText("You are looking for  "+ temp +" "+join);
+         }
+
+        intent.putExtra(EXTRA_MESSAGE, "Displaying search Results for " + join);
+        //intent.putExtra(EXTRA_MESSAGE, mText);
+        startActivity(intent);
+    }
+
 }
