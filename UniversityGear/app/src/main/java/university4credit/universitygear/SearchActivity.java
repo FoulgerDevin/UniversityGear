7a2ea2184c008809c76690a759126e07ac0faffe
liauwb231 <liauwb@onid.orst.edu>
2017-01-26 16:48:15 -0800

    insert ebay api
    P.s. I broke my gradle build and I can't run it. I tested it on a
    different project and it managed to run just fine but there might be
    some syntax error that I did not address

--- UniversityGear/app/src/main/java/university4credit/universitygear/SearchActivity.java
+++ UniversityGear/app/src/main/java/university4credit/universitygear/SearchActivity.java
@@ -25,6 +25,7 @@ public class SearchActivity extends AppCompatActivity {
     Context context;
     int mindistance;
     String tempstring;
+    String tempstr;
     String join;
     String temp;
     String [] database = {"Oregon","State", "University", "Jersey", "Mug", "Nike", "Underarmour" };
@@ -54,7 +55,7 @@ public class SearchActivity extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_search);
         mButton = (Button)findViewById(R.id.button1);
         RadioGroup radioGroup = (RadioGroup) findViewById(R.id.radiogroup);
         radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
@@ -91,7 +92,8 @@ public class SearchActivity extends AppCompatActivity {
                 for(int x = 1; x < ParsedWords.length;x++){
                     join += " "; join+=ParsedWords[x];
                 }
-
+                join.toLowerCase();
+                new asynctask().execute(join);
                 FileOutputStream stream = null;
                 try {
                     stream = openFileOutput("history.txt", Context.MODE_APPEND);
@@ -104,10 +106,104 @@ public class SearchActivity extends AppCompatActivity {
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
-                if( mEdit.getText().toString().length()!=0)
-                    mText.setText("You are looking for  "+ temp +" "+join);
+                Log.d("E","tempstr = " + tempstr);
             }
         });
 
     }
+    private class asynctask extends AsyncTask< String,Void,String> {
+        private String temp;
+        private Context tempc;
+        URL url;
+        HttpURLConnection conn;
+        InputStream inputstream;
+
+        private String getStringFromInputStream(InputStream is) {
+
+            BufferedReader br = null;
+            StringBuilder sb = new StringBuilder();
+
+            String line;
+            try {
+
+                br = new BufferedReader(new InputStreamReader(is));
+                while ((line = br.readLine()) != null) {
+                    sb.append(line);
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (br != null) {
+                    try {
+                        br.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            return sb.toString();
+
+        }
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            //this is the url that i passed in
+            String urlString = "https://api.sandbox.ebay.com/buy/browse/v1/item_summary/search?q="+params[0]+"&limit=1";
+            try {
+                url = new URL(urlString);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+            try {
+                conn = (HttpURLConnection)url.openConnection();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            //this is the header that you need to pass in. The authorization key is something you get from the eBAy website. I can tell you how to get them but I think we can just use my key
+            conn.setRequestProperty("Authorization","Bearer v^1.1#i^1#f^0#p^1#I^3#r^0#t^H4sIAAAAAAAAAOVXb2wURRTv9dorDVSNEEuIyrEStcDuze5e9+423CV3BcJByxWuNNDQwNzuXLvt3e5mZ9b2CMRatQSCiQLygUQgYkzUxA9GMRjAogKxRDGQGCV8Akn8E/BPolSCqbPXo1yrgQqnIXG/bOa9N29+7/femz+gz1M9b2DpwNUaV1X5/j7QV+5y8VNBtady/n3u8lmVZaDIwLW/b25fRb/724UYZjOmvAph09Ax8vZmMzqW88IwY1u6bECsYVmHWYRlosjJaFOjLHBANi2DGIqRYbzxRWFGRH5JqBdQQOGDKuIRleo3fLYYVA8Qr6RBIAX5kOJPC1SPsY3iOiZQJ2FGAHyABTwrSC0AyH5J9vs5EKxvY7ytyMKaoVMTDjCRPFw5P9cqwnprqBBjZBHqhInEo0uSiWh80eIVLQt9Rb4iBR6SBBIbjx81GCrytsKMjW69DM5by0lbURDGjC8yusJ4p3L0Bpg7gJ+nOqUilQ+gYEhEQBTV0lC5xLCykNwahyPRVDadN5WRTjSSux2jlI1UF1JIYbSCuogv8jq/lTbMaGkNWWFmcSy6NtrczERiVg7qjRpkV+vaUw5YNhlbw/JBqEJYn1ZZQVUFRUBiYaFRbwWaJ6zUYOiq5pCGvSsMEkMUNZrIjVjEDTVK6AkrmiYOomI74QaHImhzkjqaRZt06k5eUZYS4c0Pb5+BsdmEWFrKJmjMw0RFnqIwA01TU5mJynwtFsqnF4eZTkJM2efr6enhekTOsDp8AgC8b01TY1LpRFnIUFun10fttdtPYLV8KAptY2ovk5xJsfTSWqUA9A4mIkiSJAQKvI+HFZko/YugKGbf+I4oVYeoCPFQTKFAyq/wqhAoRYdECkXqc3CgFMyxWWh1I2JmoIJYhdaZnUWWpspifVoQg2nEqlIozfpD6TSbqlcllk8jBBBKpZRQ8P/UKJMt9YaMRpUttNRKUu8lq/WlBiZInWyt/21oScUwUbOR0ZTcfxKb0+uTjk+01GZokVzMztFxEmUy9HdX4Sr5TK4v0cZVskT+w565s9g1SO6tqHl/sF4M8RLw311c9EJzT8WlGFnO2YY5C5rEsDgKzcwgzFkIG7ZFL2BcwjmUW4xupNMtjlhGJoOsVv6uWMBOI99bPFQ8c9hxgakPaGqjlFBufAak0Tui9XnQ3skY+VJ2juuwESYUiIqsf+GA941/bkTK8h/f7zoC+l2H6IsFBADLzwd1HvfqCvc0BmsE0dzqasro5TSY5rDWodPbtIW4bpQzoWaVe1xNF7evfbroobO/Hcwce+pUu/mpRe8e8PBNTSV/f20NHwC8IAHgl/z+NvDYTW0F/1DFjMbknNPDX9RwddfnbBgm24a2XT5dDWrGjFyuyrKKfleZlT17dktd67XYxxu/unRi1pmt3yeuRwd3/dh14NWa4cG9v7wnsEd+qDUqH13+0xS845NDQ1UHL/z6xN7j298dCv38QvvziZdnzFx2+jfP1S8DUuVcor/eMzJy4ZHv9vg2hbvh+/sOL4tx5z49cPCPD3/fPXto90l34qUFtrD5jaEt5xa8dnFP+5REZ9dWZujyzJa3ZenS401RH04vOV8+snJbtafMPHlq3QffaDD+dfbUtCubau23zh99oKpBq513fMfq4MY3Z5+ZM9B+7PI7nitzr3Ufq3JdOyQ1oVWtcVF+ZfCjOvnBz1nm+mc7D3dtfnIf8EyvmL7BfaLt6K7lL67sN9cNP1u9ZWBw5LnRNP4JaptfnYIOAAA=");
+            //I added this header just so it can get the json object. I'm not sure if its necessary but can't hurt to put that in
+            conn.setRequestProperty("Accept","application/json");
+            conn.setRequestProperty("Content-Type","application/json");
+            try {
+                //since im getting item feeds, I set my request method to get
+                conn.setRequestMethod("GET");
+                //conn.setDoOutput(false);
+            } catch (ProtocolException e) {
+                e.printStackTrace();
+            }
+            int stat = 0;
+            try {
+                //this line is here to test the code. If it returns 200 then it succeeds otherwise theres something wrong with the payload
+                stat = conn.getResponseCode();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Log.d("e", "Params= "+params[0]);
+            try {
+                //get the item feed
+                inputstream = conn.getInputStream();
+                tempstr = getStringFromInputStream(inputstream);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return tempstr;
+        }
+        @Override
+        protected void onPostExecute(String strings) {
+            //I set the result on the screen for testing purpose.
+            mText.setText(tempstr);
+
+
+        }
+
+    }
+
+
 }
