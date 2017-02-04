5e948bcf8493f4f9761ac27f2fab496508a07a87
Hector Trujillo <hector.trujillo@oregonstate.edu>
2017-02-03 16:36:01 -0800

    Cleaned up Display Item Activity by removing test data.

--- UniversityGear/app/src/main/java/university4credit/universitygear/DisplaySearchResultsActivity.java
+++ UniversityGear/app/src/main/java/university4credit/universitygear/DisplaySearchResultsActivity.java
@@ -11,18 +11,10 @@ import android.widget.Toast;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.ViewGroup;
-import android.widget.TextView;
 
 public class DisplaySearchResultsActivity extends AppCompatActivity {
     private static final String TAG = "RecyclerViewExample";
@@ -44,12 +36,10 @@ public class DisplaySearchResultsActivity extends AppCompatActivity {
         mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
         progressBar = (ProgressBar) findViewById(R.id.progress_bar);
-
-        String url = "http://stacktips.com/?json=get_category_posts&slug=news&count=30";
-        new DownloadTask().execute(url);
+        new DisplayResultsTask().execute();
     }
 
-    public class DownloadTask extends AsyncTask<String, Void, Integer> {
+    public class DisplayResultsTask extends AsyncTask<String, Void, Integer> {
 
         @Override
         protected void onPreExecute() {
@@ -59,25 +49,9 @@ public class DisplaySearchResultsActivity extends AppCompatActivity {
         @Override
         protected Integer doInBackground(String... params) {
             Integer result = 0;
-            HttpURLConnection urlConnection;
             try {
-                URL url = new URL(params[0]);
-                urlConnection = (HttpURLConnection) url.openConnection();
-                int statusCode = urlConnection.getResponseCode();
-
-                // 200 represents HTTP OK
-                if (statusCode == 200) {
-                    BufferedReader r = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
-                    StringBuilder response = new StringBuilder();
-                    String line;
-                    while ((line = r.readLine()) != null) {
-                        response.append(line);
-                    }
-                    parseResult(response.toString());
+                    parseResult(searchResults);
                     result = 1; // Successful
-                } else {
-                    result = 0; //"Failed to fetch data!";
-                }
             } catch (Exception e) {
                 Log.d(TAG, e.getLocalizedMessage());
             }
@@ -95,19 +69,17 @@ public class DisplaySearchResultsActivity extends AppCompatActivity {
                     @Override
                     public void onItemClick(FeedItem item) {
                         Toast.makeText(DisplaySearchResultsActivity.this, item.getTitle(), Toast.LENGTH_LONG).show();
-
                     }
                 });
-
             } else {
                 Toast.makeText(DisplaySearchResultsActivity.this, "Failed to fetch data!", Toast.LENGTH_SHORT).show();
             }
         }
     }
 
-    private void parseResult(String result) {
+    private void parseResult(String results) {
         try {
-            JSONObject response = new JSONObject(searchResults);
+            JSONObject response = new JSONObject(results);
             JSONArray posts = response.optJSONArray("itemSummaries");
             feedsList = new ArrayList<>();
 
