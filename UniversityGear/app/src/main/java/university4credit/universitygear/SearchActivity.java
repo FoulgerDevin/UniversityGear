6953cd282e36699a4e364e75fd4460bd105642a8
liauwb231 <liauwb@onid.orst.edu>
2017-02-08 23:24:33 -0800

    Added some buttons and checkboxes and oauthtoken

--- UniversityGear/app/src/main/java/university4credit/universitygear/SearchActivity.java
+++ UniversityGear/app/src/main/java/university4credit/universitygear/SearchActivity.java
@@ -41,9 +41,11 @@ public class SearchActivity extends AppCompatActivity {
     int mindistance;
     String tempstring;
     String tempstr;
+    String tempid;
     String join;
     String [] database = {"Oregon","State", "University", "Jersey", "Mug", "Nike", "Underarmour" };
     public List<Item> itemFeed = null;
+
     // Is the button now checked?
 
 
@@ -72,8 +74,30 @@ public class SearchActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
         mButton = (Button)findViewById(R.id.button1);
+        RadioGroup radioGroup = (RadioGroup) findViewById(R.id.radiogroup);
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId)
+            {
+                RadioButton checkedRadioButton = (RadioButton) findViewById(checkedId);
+                temp = "filter=conditions:{"+checkedRadioButton.getText().toString()+"}";
+            }
+        });
         mButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
+                if(box1.isChecked()){
+                    tempid += "&category_id=2228";
+                }
+                if(box2.isChecked()){
+                    tempid += "&category_id=11450";
+                }
+                if(box3.isChecked()){
+                    tempid += "&category_id=888";
+                }
+                if(box4.isChecked()){
+                    tempid += "&category_id=64882";
+                }
                 mEdit = (EditText)findViewById(R.id.editText1);
                 String [] ParsedWords = mEdit.getText().toString().split("\\s+") ;
                 for(int x = 0; x < ParsedWords.length;x++){
@@ -117,11 +141,12 @@ public class SearchActivity extends AppCompatActivity {
     }
 
     private class SearchItemTask extends AsyncTask<String,Void,List<Item>> {
-        private String temp;
+        private String temp, oAuthtoken;
         private Context tempc;
         private String itemLimit = "200";
-        URL url;
-        HttpURLConnection conn;
+        URL url, url2;
+        HttpURLConnection conn,conn2;
+        JSONObject json;
         InputStream inputstream;
         String result = "";
 
@@ -158,10 +183,17 @@ public class SearchActivity extends AppCompatActivity {
 
         @Override
         protected List<Item> doInBackground(String... params) {
-            //this is the url that i passed in
-            String urlString = "https://api.sandbox.ebay.com/buy/browse/v1/item_summary/search?q="+params[0]+"&limit=" + itemLimit;
+            String key;
+            String id = "BryanLia-Universi-SBX-18adaa5fd-2dd2c2e3:SBX-8adaa5fd4c3c-8c04-4d13-9adb-8196";
+            byte[] data = new byte[0];
             try {
-                url = new URL(urlString);
+                data = id.getBytes("UTF-8");
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+            String base64 = Base64.encodeToString(data, Base64.NO_WRAP);
+            try {
+                url = new URL("https://api.sandbox.ebay.com/identity/v1/oauth2/token");
             } catch (MalformedURLException e) {
                 e.printStackTrace();
             }
@@ -171,31 +203,74 @@ public class SearchActivity extends AppCompatActivity {
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            //this is the header that you need to pass in. The authorization key is something you get from the eBAy website. I can tell you how to get them but I think we can just use my key
-            //conn.setRequestProperty("Authorization", "Bearer " + context.getString(R.string.OAuth));
-            conn.setRequestProperty("Authorization","Bearer v^1.1#i^1#r^0#p^3#I^3#f^0#t^H4sIAAAAAAAAAOVXa2wUVRTu9mmpgCgUgqjrQA0Is3N3Xrs7tBu3D9IqLZVtkUdJuTNzpx06O7PO3Gm7P4xNTUjU2hBFYkxFYgjRQBCJCIRgQoygIVER5YeIMQoJEKKB+IiPoHe2D7Y1lj5IbOL+2dx7z+s75ztn7gVd+YUPb6ne8ut0X0H2zi7Qle3zBYtAYX7e0hk52fPzskCGgG9n16Ku3O6cS6UOTBhJaTVykpbpIH9nwjAdKb1ZRrm2KVnQ0R3JhAnkSFiR4rHalRIbAFLStrClWAblr6kso3hWC4U4UQsDkdU4kSe75qDNBquMgoLIKkDlOSDKSAXeueO4qMZ0MDRxGcWCYIgGLA34BsBKAisBLsALwnrKvwbZjm6ZRCQAqGg6XCmta2fEOnqo0HGQjYkRKloTWxFfFauprKprKGUybEUH8hDHELvO8FWFpSL/Gmi4aHQ3TlpairuKghyHYqL9HoYblWKDwUwg/HSqIeA0ThAioiYICgTabUnlCstOQDx6HN6OrtJaWlRCJtZx6lYZJdmQNyMFD6zqiImaSr/397gLDV3TkV1GVZXH1jXGq1ZT/nh9vW216ypSPaRBjuNCohgSqChGDkkhspux7W7WDaN1wFe/wYFMj3BWYZmq7uXN8ddZuByRwNHI9PAZ6SFCq8xVdkzDXlCZcvxgGrnQeq+u/YV0cavplRYlSC786eWtizDIips8uF284HkV8SoPIS9yMKLKw3nh9frEuBH1yhOrr2eQDFN0AtptCCcNqCBaIal1E8jWVYnnZV6EikZzbFiheVVQ6EhEQDSLRCSykOOBxv/P6IGxrcsuRkMUGXmQxllGxRUrieotQ1dS1EiR9NQZIESnU0a1YpyUGKajoyPQwQUsu4VhAQgya2tXxpVWlIDUkKx+a2FaT1NDQUTL0SWcSpJoOgnziHOzhYpytloPbZwqd1NkHUeGQf4G2TsswujI3X+B6nhQpxZIT98hBmBSD3gEDyhWgrEgaWZvqzkdsX8sQozspoh/FdkBG0HVMo3U2PVaXELgfu2xKTmkGoH+XiQwRnj0en18BsbhVDfbCZctOzVOmMOVx6EDFcVyTTwRdwOq49DQXEMj88Nr14k4zFAfT5gmNFJYV5whl5PqslgyWaNOrS7jK2xEhi3daOrt3oeGjpevpZEqQ5ZHMqKBjFhNZoOTgq2idl1BzfoUg266hjEpXLUto0Eivd73X8CqY2KTQlWJ2qcaSVVVDEV4WabDcojcYQQ5REM5zNMoIkZUQlOgyGBSmCsMnQyGhtRU+wZWWw5G6uSgkZvo1ALlTZjBARMMhzUyZFhIbqaIpcOCwtOywAtjhTxiI+NG94+7PDP8PR3NSv+C3b5joNt3hDzJQQjQwaVgSX5OY27OnZSjYxRwoKnKVmdAh1rA0VtM8ly0UaANpZJQt7PzfbXf96x7OuMlv3MjmDf0li/MCRZlPOzBgpsnecGZc6cHQ4AFPGAFFnDrwcKbp7nB4tzZXb+lmJJ7C64evdr+7plDz9GrwVsHwfQhIZ8vLyu325dloctP3ld46LXzPy2/uGH3S7PfW1z92fKi/Ve22qeSkXXijb4Dd13/iKbVZOxgzpxi+50Fuy+UvPCDce3krsRXbfDRFw+//Ym+49w9l5c9e3fTK00tG/Rvc6xzZ1Mf7+OKe2Kbe37f09Z0ve0h9MiRkudbfnn/+KXikl19s56KLNq7uGneiS8j+MeZG32vSqUb/kLHGa1V3fvn5e/mMdO23XFy7ukLfZ+/sWba1otLvv5jznH+xs8P8Eu3Hznx5rEPl8SeaM4veP3YF9fA/mV7Zh9daxeFZ/RemNV8+FRv1fbzXKKUb7vSs4J5EG5pvRGfHzzT+8yBgtOb7m/s+6Cx+tqmHWc//Wbbvo0vLyx97Hxlfxn/BlwEOvJjEQAA");
-            //I added this header just so it can get the json object. I'm not sure if its necessary but can't hurt to put that in
-            conn.setRequestProperty("Accept","application/json");
-            conn.setRequestProperty("Content-Type","application/json");
+            conn.setRequestProperty("Authorization","Basic "+base64);
+            conn.setRequestProperty("Content-Type","application/x-www-form-urlencoded");
+            conn.setDoOutput(true);
             try {
-                //since im getting item feeds, I set my request method to get
-                conn.setRequestMethod("GET");
+                conn.setRequestMethod("POST");
                 //conn.setDoOutput(false);
             } catch (ProtocolException e) {
                 e.printStackTrace();
             }
-            int stat;
+
+            String urlParameters  = "grant_type=client_credentials&redirect_uri=Bryan_Liauw-BryanLia-Univer-kheulrrfh&scope=https://api.ebay.com/oauth/api_scope";
+            byte[] postData       = urlParameters.getBytes( StandardCharsets.UTF_8 );
+            conn.setRequestProperty( "Content-Length", Integer.toString( postData.length ));
             try {
-                //this line is here to test the code. If it returns 200 then it succeeds otherwise theres something wrong with the payload
-                stat = conn.getResponseCode();
-                Log.e("Connection code", " " + stat);
+                conn.getOutputStream().write(postData);
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            Log.d("e", "Params= "+params[0]);
+
+
+            try {
+                BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                StringBuilder sb = new StringBuilder();
+                String line;
+                while ((line = br.readLine()) != null) {
+                    sb.append(line+"\n");
+                }
+                br.close();
+
+                json = new JSONObject(sb.toString());
+                oAuthtoken = json.getString("access_token");
+                Log.d("hey", oAuthtoken);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                Log.d("e","asdf");
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            //this is the url that i passed in
+            String urlString = "https://api.sandbox.ebay.com/buy/browse/v1/item_summary/search?q="+params[0]+tempid+"&limit=" + itemLimit;
+            try {
+                url2 = new URL(urlString);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+            try {
+                conn2 = (HttpURLConnection)url.openConnection();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            //this is the header that you need to pass in. The authorization key is something you get from the eBAy website. I can tell you how to get them but I think we can just use my key
+            //conn.setRequestProperty("Authorization", "Bearer " + context.getString(R.string.OAuth));
+            conn2.setRequestProperty("Authorization","Bearer "+oAuthtoken);
+            //I added this header just so it can get the json object. I'm not sure if its necessary but can't hurt to put that in
+            conn2.setRequestProperty("Accept","application/json");
+            conn2.setRequestProperty("Content-Type","application/json");
+            try {
+                //since im getting item feeds, I set my request method to get
+                conn2.setRequestMethod("GET");
+                //conn.setDoOutput(false);
+            } catch (ProtocolException e) {
+                e.printStackTrace();
+            }
             try {
                 //get the item feed
-                inputstream = conn.getInputStream();
+                inputstream = conn2.getInputStream();
                 result = getStringFromInputStream(inputstream);
             } catch (IOException e) {
                 e.printStackTrace();
