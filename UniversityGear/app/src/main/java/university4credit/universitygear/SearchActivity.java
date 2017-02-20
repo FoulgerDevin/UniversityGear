bbabbb4850e3a7f8ab005878279f810b0983867b
foulgerdevin <foulgerdevin@gmail.com>
2017-02-20 15:57:27 -0800

    Removing changes because they broke our build.

--- UniversityGear/app/src/main/java/university4credit/universitygear/SearchActivity.java
+++ UniversityGear/app/src/main/java/university4credit/universitygear/SearchActivity.java
@@ -9,21 +9,16 @@ import android.os.Bundle;
 import android.content.Context;
 import android.os.Bundle;
 import android.app.Activity;
-import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
-import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -32,12 +27,10 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
-import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 public class SearchActivity extends AppCompatActivity {
@@ -46,18 +39,12 @@ public class SearchActivity extends AppCompatActivity {
     EditText mEdit;
     TextView mText;
     String newtext;
-    String schoolname;
     Context context;
     int mindistance;
+    String tempstring;
+    String tempstr;
     String join;
-    String temp, tempstr, tempstring, categoryID;
-    String tempid = "";
-    String oAuthtoken;
-    String price;
     String [] database = {"Oregon","State", "University", "Jersey", "Mug", "Nike", "Underarmour" };
-    CheckBox box1, box2, box3, box4;
-    JSONObject json;
-    TextView num1,num2;
     public List<Item> itemFeed = null;
     private ProgressBar progressBar;
     // Is the button now checked?
@@ -95,58 +82,6 @@ public class SearchActivity extends AppCompatActivity {
                 hideSoftKeyboard();
                 progressBar.setVisibility(View.VISIBLE);
                 mEdit = (EditText)findViewById(R.id.editText1);
-                box1 = (CheckBox)findViewById(R.id.checkbox1);
-                box2 = (CheckBox)findViewById(R.id.checkbox2);
-                box3 = (CheckBox)findViewById(R.id.checkbox3);
-                box4 = (CheckBox)findViewById(R.id.checkbox4);
-                mButton = (Button)findViewById(R.id.button1);
-                temp = "filter=conditions:{UNSPECIFIED}";
-                RadioGroup radioGroup = (RadioGroup) findViewById(R.id.radiogroup);
-                radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-
-                    @Override
-                    public void onCheckedChanged(RadioGroup group, int checkedId)
-                    {
-                        RadioButton checkedRadioButton = (RadioButton) findViewById(checkedId);
-                        temp = "filter=conditions:{"+checkedRadioButton.getText().toString()+"}";
-                    }
-                });
-                Log.d("aasdf",schoolname);
-                mButton.setOnClickListener(new View.OnClickListener() {
-                    public void onClick(View view) {
-                        if(box1.isChecked()){
-                            tempid += "&category_id=2228";
-                        }
-                        if(box2.isChecked()){
-                            tempid += "&category_id=11450";
-                        }
-                        if(box3.isChecked()){
-                            tempid += "&category_id=888";
-                        }
-                        if(box4.isChecked()){
-                            tempid += "&category_id=64882";
-                        }
-                        mEdit   = (EditText)findViewById(R.id.editText1);
-                        mText = (TextView)findViewById(R.id.textView1);
-                        num1 = (EditText)findViewById(R.id.editText);
-                        num2 = (EditText)findViewById(R.id.editText2);
-
-                        if(num1.length()!=0){
-                            if(num2.length()!=0){
-                                price = "&filter=priceCurrency:USD&filter=price:["+num1.getText().toString()+".."+num2.getText().toString()+"]";
-                            }
-                            else{
-                                price = "&filter=priceCurrency:USD&filter=price:["+num1.getText().toString()+"]";
-                            }
-                        }
-                        else{
-                            if(num2.length()!=0){
-                                price = "&filter=priceCurrency:USD&filter=price:[.."+num2.getText().toString()+"]";
-                            }
-                            else{
-                                price = "";
-                            }
-                        }
                 String [] ParsedWords = mEdit.getText().toString().split("\\s+") ;
                 for(int x = 0; x < ParsedWords.length;x++){
                     mindistance = 3;
@@ -188,12 +123,12 @@ public class SearchActivity extends AppCompatActivity {
 
     }
 
-   class SearchItemTask extends AsyncTask<String,Void,List<Item>> {
+    private class SearchItemTask extends AsyncTask<String,Void,List<Item>> {
         private String temp;
         private Context tempc;
         private String itemLimit = "200";
-        URL url, url2;
-        HttpURLConnection conn, conn2;
+        URL url;
+        HttpURLConnection conn;
         InputStream inputstream;
         String result = "";
 
@@ -230,16 +165,10 @@ public class SearchActivity extends AppCompatActivity {
 
         @Override
         protected List<Item> doInBackground(String... params) {
-            String id = "BryanLia-Universi-SBX-18adaa5fd-2dd2c2e3:SBX-8adaa5fd4c3c-8c04-4d13-9adb-8196";
-            byte[] data = new byte[0];
+            //this is the url that i passed in
+            String urlString = "https://api.sandbox.ebay.com/buy/browse/v1/item_summary/search?q="+params[0]+"&limit=" + itemLimit;
             try {
-                data = id.getBytes("UTF-8");
-            } catch (UnsupportedEncodingException e) {
-                e.printStackTrace();
-            }
-            String base64 = Base64.encodeToString(data, Base64.NO_WRAP);
-            try {
-                url = new URL("https://api.sandbox.ebay.com/identity/v1/oauth2/token");
+                url = new URL(urlString);
             } catch (MalformedURLException e) {
                 e.printStackTrace();
             }
@@ -249,96 +178,32 @@ public class SearchActivity extends AppCompatActivity {
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            conn.setRequestProperty("Authorization","Basic "+base64);
-            conn.setRequestProperty("Content-Type","application/x-www-form-urlencoded");
-            conn.setDoOutput(true);
+
+            //this is the header that you need to pass in. The authorization key is something you get from the eBAy website. I can tell you how to get them but I think we can just use my key
+            conn.setRequestProperty("Authorization", "Bearer " + getString(R.string.appKey));
+
+            //I added this header just so it can get the json object. I'm not sure if its necessary but can't hurt to put that in
+            conn.setRequestProperty("Accept","application/json");
+            conn.setRequestProperty("Content-Type","application/json");
             try {
-                conn.setRequestMethod("POST");
+                //since im getting item feeds, I set my request method to get
+                conn.setRequestMethod("GET");
                 //conn.setDoOutput(false);
             } catch (ProtocolException e) {
                 e.printStackTrace();
             }
-
-            String urlParameters  = "grant_type=client_credentials&redirect_uri=Bryan_Liauw-BryanLia-Univer-kheulrrfh&scope=https://api.ebay.com/oauth/api_scope";
-            byte[] postData       = urlParameters.getBytes( StandardCharsets.UTF_8 );
-            conn.setRequestProperty( "Content-Length", Integer.toString( postData.length ));
+            int stat;
             try {
-                conn.getOutputStream().write(postData);
+                //this line is here to test the code. If it returns 200 then it succeeds otherwise theres something wrong with the payload
+                stat = conn.getResponseCode();
+                Log.e("Connection code", " " + stat);
             } catch (IOException e) {
                 e.printStackTrace();
             }
-
-
-            try {
-                BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-                StringBuilder sb = new StringBuilder();
-                String line;
-                while ((line = br.readLine()) != null) {
-                    sb.append(line+"\n");
-                }
-                br.close();
-
-                json = new JSONObject(sb.toString());
-                oAuthtoken = json.getString("access_token");
-                Log.d("hey", oAuthtoken);
-
-            } catch (IOException e) {
-                e.printStackTrace();
-                Log.d("e","asdf");
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-            Log.d("Price", price);
-            try {
-                url2 = new URL("https://api.sandbox.ebay.com/buy/browse/v1/item_summary/search?q="+schoolname+params[0]+tempid+price+"&filter=buyingOptions:(FIXED_PRICE)&limit=1");
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            }
-
-            try {
-                conn2 = (HttpURLConnection)url.openConnection();
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            conn2.setRequestProperty("Authorization","Bearer "+oAuthtoken);
-            conn2.setRequestProperty("Accept","application/json");
-            conn2.setRequestProperty("Content-Type","application/json");
-            try {
-                conn2.setRequestMethod("GET");
-                //conn.setDoOutput(false);
-            } catch (ProtocolException e) {
-                e.printStackTrace();
-            }
-
-
-            //this is the url that i passed in
-            try {
-                url2 = new URL("https://api.sandbox.ebay.com/buy/browse/v1/item_summary/search?q="+schoolname+params[0]+tempid+price+"&filter=buyingOptions:(FIXED_PRICE)&limit=1");
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            }
-
-            try {
-                conn2 = (HttpURLConnection)url.openConnection();
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            conn2.setRequestProperty("Authorization","Bearer "+oAuthtoken);
-            conn2.setRequestProperty("Accept","application/json");
-            conn2.setRequestProperty("Content-Type","application/json");
-            try {
-                conn2.setRequestMethod("GET");
-                //conn.setDoOutput(false);
-            } catch (ProtocolException e) {
-                e.printStackTrace();
-            }
-
             Log.d("e", "Params= "+params[0]);
             try {
                 //get the item feed
-                inputstream = conn2.getInputStream();
+                inputstream = conn.getInputStream();
                 result = getStringFromInputStream(inputstream);
             } catch (IOException e) {
                 e.printStackTrace();
