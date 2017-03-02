b67b56359481bf5576dd4642444e074a5c3a9cb1
liauwb231 <liauwb@onid.orst.edu>
2017-03-02 04:30:30 -0800

    Added location based suggestion for school selection

--- UniversityGear/app/src/main/java/university4credit/universitygear/SearchActivity2.java
+++ UniversityGear/app/src/main/java/university4credit/universitygear/SearchActivity2.java
@@ -1,16 +1,29 @@
 package university4credit.universitygear;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ToggleButton;
 
-public class SearchActivity2 extends Activity {
+import java.util.ArrayList;
+
+public class SearchActivity2 extends Activity implements LocationListener {
     ToggleButton button1,button2,button3;
+    ArrayList<SchoolList> list = new ArrayList<SchoolList>();
+    double mindistance = 99999999999.;
+    int min;
     Button mbutton, mbutton2;
     EditText edit;
     int temp1, temp2, temp3;
@@ -19,9 +32,28 @@ public class SearchActivity2 extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search2);
+        if(ContextCompat.checkSelfPermission(SearchActivity2.this, Manifest.permission.ACCESS_FINE_LOCATION)!=PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(SearchActivity2.this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},1);
+        }
+
+        TrackGPS gps = new TrackGPS(SearchActivity2.this);
+        SchoolList list = gps.getSchoolList();
+
+                // Called when a new location is found by the network location provider.
+
+
         button1 = (ToggleButton)findViewById(R.id.toggleButton);
         button2 = (ToggleButton)findViewById(R.id.toggleButton2);
         button3 = (ToggleButton)findViewById(R.id.toggleButton3);
+        button1.setTextOn(list.Schools[0]);
+        button2.setTextOn(list.Schools[1]);
+        button3.setTextOn(list.Schools[2]);
+        button1.setText(list.Schools[0]);
+        button2.setText(list.Schools[1]);
+        button3.setText(list.Schools[2]);
+        button1.setTextOff(list.Schools[0]);
+        button2.setTextOff(list.Schools[1]);
+        button3.setTextOff(list.Schools[2]);
         mbutton = (Button)findViewById(R.id.nextbutton);
         mbutton2 = (Button)findViewById(R.id.nextbutton2);
         edit = (EditText)findViewById(R.id.editText1);
@@ -62,4 +94,23 @@ public class SearchActivity2 extends Activity {
 
     }
 
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
 }
